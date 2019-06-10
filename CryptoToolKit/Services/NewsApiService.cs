3fa7dd75553ffaa61e812bf3fd46f1b953028810
Dalton Brady <dalton.brady@ioflex.net>
2019-06-10 11:05:56 -0500

    Added News API service. Added ConvertPrice API controller action.

---
+++ CryptoToolKit/Services/NewsApiService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CryptoToolKit.Services
+{
+    public class NewsApiService
+    {
+
+        public HttpClient Client { get; set; }
+
+        public NewsApiService(HttpClient client)
+        {
+            client.BaseAddress = new Uri("https://newsapi.org/v2/");
+            client.DefaultRequestHeaders.Add("X-Api-Key", "af402fb386a04966b4c438dccdb1dcb1");
+
+            this.Client = client;
+        }
+    }
+}
