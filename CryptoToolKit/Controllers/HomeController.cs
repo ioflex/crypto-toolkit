79f336a0086695cbde62e605858b109e2df5aeda
Dalton Brady <dalton.brady@ioflex.net>
2019-06-11 18:24:30 -0500

    Added local fontawesome / jquery-chosen resources to wwwroot/lib
    Moved the "Dashboard" logic (Views/ Controller) into the Dashboard area, still need to refactor models into the area as well.
    
    Added the news sidebar into the dashboard and tested intital viewing / hiding of it.

--- CryptoToolKit/Controllers/HomeController.cs
+++ CryptoToolKit/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+
 using Microsoft.AspNetCore.Mvc;
+
 using CryptoToolKit.Models;
-using CryptoToolKit.Services;
 
 namespace CryptoToolKit.Controllers
 {
@@ -13,12 +11,9 @@ namespace CryptoToolKit.Controllers
         /// <summary>
         /// CTOR
         /// </summary>
-        /// <param name="cmcService"></param>
-        /// <param name="naService"></param>
-        public HomeController(CoinMarketCapService cmcService, NewsApiService naService)
+        public HomeController()
         { 
-            // TODO: Implement user settings ex: default fiat currency.
-            this.Initialize(cmcService, naService);
+
         }
 
         /// <summary>
@@ -65,71 +60,6 @@ namespace CryptoToolKit.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        /// <summary>
-        /// Returns the amount specified by <paramref name="amount"/> of the symbol specified by <paramref name="symbol"/>
-        /// in the specified set of symbols <paramref name="toSymbols"/>
-        /// </summary>
-        /// <param name="amount"></param>
-        /// <param name="symbol"></param>
-        /// <param name="toSymbols"></param>
-        /// <returns></returns>
-        [HttpGet]
-        public async Task<JsonResult> ConvertPrice(double amount, string symbol, string[] toSymbols)
-        {
-            try
-            {
-                var priceConversion = await this.CoinMarketCapService.ConvertPriceAsync(amount, symbol, toSymbols);
-
-                return OkJson(priceConversion);
-            }
-            catch (Exception e)
-            {
-                // TODO: Document Exception
-                return ServerExceptionJson(e);
-            }
-        }
-
-        /// <summary>
-        /// Returns the median price for crypto currency, in specified fiat.
-        /// </summary>
-        /// <param name="asset"></param>
-        /// <param name="fiat"></param>
-        /// <returns></returns>
-        [HttpGet]
-        public async Task<JsonResult> CurrentCryptoPrice(string symbol, string fiat)
-        {
-            try
-            {
-                var tickerSymbol = await this.CoinMarketCapService.GetTickerAsync(symbol ?? "BTC", fiat);
-
-                return OkJson(tickerSymbol);
-            }
-            catch (Exception e)
-            {
-                // TODO: Document Exception
-                return ServerExceptionJson(e);
-            }
-        }
-
-        /// <summary>
-        /// TODO: Controller Action Documentations
-        /// </summary>
-        /// <returns></returns>
-        public async Task<JsonResult> CurrentListings()
-        {
-            try
-            {
-                var listings = await this.CoinMarketCapService.GetListingsAsync();
-
-                return OkJson(listings);
-            }
-            catch (Exception e)
-            {
-                // TODO: Document Exception
-                return ServerExceptionJson(e);
-            }
         }
     }
 }
--- CryptoToolKit/Startup.cs
+++ CryptoToolKit/Startup.cs
@@ -71,17 +71,14 @@ namespace CryptoToolKit
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                  name: "areas",
-                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-                );
-            });
         }
     }
 }
